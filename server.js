cb56d884c76df92722497d0b22ebc3715715a804
Aditya Raj Singh <166025105+Adityarajsingh2904@users.noreply.github.com>
2025-06-27 04:55:44 +0530

    Remove stray debug logs

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ require('./models/Article');
 require('./models/Comment');
 require('./config/passport');
 
-const server = app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on port ' + server.address().port);
-});
+const server = app.listen(process.env.PORT || 3000, function () {});
 
 module.exports = server;
