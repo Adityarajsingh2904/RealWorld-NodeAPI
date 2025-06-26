bb18bbcf6b200a4f258c9e6a74f20c55242c29e2
Aditya Raj Singh <166025105+Adityarajsingh2904@users.noreply.github.com>
2025-06-27 00:56:24 +0530

    refactor: modularize express setup

---
+++ config/app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(
+  session({
+    secret: 'conduit',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+app.use(require('../routes'));
+
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (!isProduction) {
+  app.use(function (err, req, res, next) {
+    console.log(err.stack);
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+module.exports = app;
